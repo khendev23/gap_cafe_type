f79ee86c8bbf1597ba1f818ff361d1e583929d4d
yu_seongkeun <khendev23@gmail.com>
2025-12-01 09:48:20 +0900

    251201 주석 수정

--- src/app/api/idle/route.ts
+++ src/app/api/idle/route.ts
@@ -1,7 +1,6 @@
-// app/api/menus/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import type { Pool, RowDataPacket } from "mysql2/promise";
-import getDbPool from "@/util/dbConnect"; // ← TS라면 .js 빼세요. JS면 그대로 사용 가능
+import getDbPool from "@/util/dbConnect";
 
 // mysql2 사용 시 Edge 런타임 불가 -> Node.js 런타임 강제
 export const runtime = "nodejs";
